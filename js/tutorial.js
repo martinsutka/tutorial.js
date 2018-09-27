7ccec965c679b1e06cdd45fc6415727768f0de64
xxxmatko <martin.sutka@gmail.com>
2018-09-27 13:11:12 +0200

    Git ignore Create tutorial container element Added reference to tutorial style

--- js/tutorial.js
+++ js/tutorial.js
@@ -8,6 +8,31 @@
         root.tutorial = factory(root.jQuery);
     }
 }(typeof (self) !== "undefined" ? self : this, function ($) {
+    //#region [ Fields ]
+    
+    var doc = (function () { return this; })().document;
+    
+    //#endregion
+
+
+    //#region [ Methods : Private ]
+
+    /**
+     * Creates container element and appends it to the body.
+     * 
+     * @returns {HTMLElement} Container element.
+     */
+    function _createContainer() {
+        var el = doc.createElement("div");
+        el.className = "tutorial";
+
+        doc.body.appendChild(el);
+
+        return el;
+    }
+
+    //#endregion
+
     //#region [ Methods : Public ]
 
     /**
@@ -24,6 +49,19 @@
             console.error("Unable to start empty tutorial.");
             return;
         }
+        
+        // var dfr = $.Deferred();
+        // var handler = $this._onEscape.bind($this, dfr);
+        var container = _createContainer();
+        
+        // $(doc).on("keydown", handler);
+
+        // $this._createStep(0, t.steps, dfr, t.scope);
+
+        // dfr.promise().then(function () {
+        //     $(doc).off("keydown", handler);
+        //     container.remove();
+        // }); 
     }
 
     //#endregion
