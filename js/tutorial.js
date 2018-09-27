94c85f469f061a1c8c90b711b4cdd89081dd3e6e
xxxmatko <martin.sutka@gmail.com>
2018-09-27 14:06:22 +0200

    Tutorial constructor and start method

--- js/tutorial.js
+++ js/tutorial.js
@@ -33,7 +33,30 @@
 
     //#endregion
 
-    //#region [ Methods : Public ]
+
+    //#region [ Event Handlers ]
+
+    /**
+     * Handles 'Escape' keydown event.
+     * 
+     * @param {function} resolve Function to resolve the main promise.
+     * @param {obejt} e Event arguments.
+     */
+    function _onEscape(resolve, e) {
+        var kc = (e.which ? e.which : e.keyCode);
+
+        if (kc === 27) {
+            resolve();
+            return false;
+        }
+
+        return true;
+    }
+
+    //#endregion
+
+
+    //#region [ Constructor ]
 
     /**
      * Starts the tutorial.
@@ -41,30 +64,65 @@
      * @param {array} steps Tutorial steps.
      * @param {object} options Options.
      */
-    function tutorial(steps, options) {
-        steps = steps || [];
-        options = options || {};
+    var Tutorial = function(steps, options) {
+        this.steps = steps || [];
+        this.options = options || {};
+        this.container = null;
+        this.promise = null;
 
-        if(!(steps instanceof Array) || (steps.length <= 0)) {
-            console.error("Unable to start empty tutorial.");
+        this._onEscapeHandler = null;
+
+        if(!(this.steps instanceof Array) || (this.steps.length <= 0)) {
+            console.error("Unable to create empty tutorial.");
             return;
         }
-        
-        // var dfr = $.Deferred();
-        // var handler = $this._onEscape.bind($this, dfr);
-        var container = _createContainer();
-        
-        // $(doc).on("keydown", handler);
+    };
 
-        // $this._createStep(0, t.steps, dfr, t.scope);
+    //#endregion
 
-        // dfr.promise().then(function () {
-        //     $(doc).off("keydown", handler);
-        //     container.remove();
-        // }); 
+
+    //#region [ Methods : Public ]
+
+    /**
+     * Starts the tutorial.
+     * 
+     * @returns {Promise} Promise which is resolved when the tutorial ends.
+     */
+    Tutorial.prototype.start = function() {
+        var $this = this;
+
+        // Create container
+        this.container = _createContainer();
+
+        // Create main promise
+        this.promise = new Promise(function(resolve) {
+            $this._onEscapeHandler = _onEscape.bind($this, resolve);
+            doc.addEventListener("keydown", $this._onEscapeHandler);
+            // $this._createStep(0, t.steps, dfr, t.scope);
+        });
+
+        // Clean things when the tutorial ends
+        this.promise.then(function() {
+            doc.removeEventListener("keydown", $this._onEscapeHandler);
+            doc.body.removeChild($this.container);
+        });
+
+        return this.promise;
+    };
+
+
+    /**
+     * Creates new intance of the Tutorial.
+     * 
+     * @returns {Tutorial} New tutorial.
+     */
+    function ctor() {
+        var args = Array.prototype.slice.call(arguments);
+        return new Tutorial(args[0], args[1]);
     }
 
     //#endregion
 
-    return tutorial;
+
+    return ctor;
 }));
